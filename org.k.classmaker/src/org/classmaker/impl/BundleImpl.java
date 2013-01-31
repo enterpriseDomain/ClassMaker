6fed9c044f5e88b89b0b183a2ce3789e1e804360
Kirill Zotkin <kirillzotkin@gmail.com>
2013-01-31 08:19:47 +0700

    set bundle ok status
    Signed-off-by: Kirill Zotkin <kirillzotkin@gmail.com>

--- org.k.classmaker/src/org/classmaker/impl/BundleImpl.java
+++ org.k.classmaker/src/org/classmaker/impl/BundleImpl.java
@@ -15,6 +15,7 @@ import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
@@ -31,20 +32,22 @@ import org.eclipse.emf.ecore.impl.EObjectImpl;
  * <p>
  * The following features are implemented:
  * <ul>
- *   <li>{@link org.classmaker.impl.BundleImpl#getName <em>Name</em>}</li>
- *   <li>{@link org.classmaker.impl.BundleImpl#getEPackage <em>EPackage</em>}</li>
- *   <li>{@link org.classmaker.impl.BundleImpl#getDynamicEPackage <em>Dynamic EPackage</em>}</li>
- *   <li>{@link org.classmaker.impl.BundleImpl#getState <em>State</em>}</li>
- *   <li>{@link org.classmaker.impl.BundleImpl#getStatus <em>Status</em>}</li>
+ * <li>{@link org.classmaker.impl.BundleImpl#getName <em>Name</em>}</li>
+ * <li>{@link org.classmaker.impl.BundleImpl#getEPackage <em>EPackage</em>}</li>
+ * <li>{@link org.classmaker.impl.BundleImpl#getDynamicEPackage <em>Dynamic
+ * EPackage</em>}</li>
+ * <li>{@link org.classmaker.impl.BundleImpl#getState <em>State</em>}</li>
+ * <li>{@link org.classmaker.impl.BundleImpl#getStatus <em>Status</em>}</li>
  * </ul>
  * </p>
- *
+ * 
  * @generated
  */
 public class BundleImpl extends EObjectImpl implements Bundle {
 	/**
 	 * The default value of the '{@link #getName() <em>Name</em>}' attribute.
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @see #getName()
 	 * @generated
 	 * @ordered
@@ -54,6 +57,7 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 	/**
 	 * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @see #getName()
 	 * @generated
 	 * @ordered
@@ -61,8 +65,9 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 	protected String name = NAME_EDEFAULT;
 
 	/**
-	 * The cached value of the '{@link #getEPackage() <em>EPackage</em>}' containment reference.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * The cached value of the '{@link #getEPackage() <em>EPackage</em>}'
+	 * containment reference. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @see #getEPackage()
 	 * @generated
 	 * @ordered
@@ -70,9 +75,10 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 	protected EPackage ePackage;
 
 	/**
-	 * The cached value of the '{@link #getDynamicEPackage() <em>Dynamic EPackage</em>}' containment reference.
-	 * <!-- begin-user-doc
+	 * The cached value of the '{@link #getDynamicEPackage()
+	 * <em>Dynamic EPackage</em>}' containment reference. <!-- begin-user-doc
 	 * --> <!-- end-user-doc -->
+	 * 
 	 * @see #getDynamicEPackage()
 	 * @generated
 	 * @ordered
@@ -82,6 +88,7 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 	/**
 	 * The default value of the '{@link #getState() <em>State</em>}' attribute.
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @see #getState()
 	 * @generated
 	 * @ordered
@@ -91,6 +98,7 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 	/**
 	 * The cached value of the '{@link #getState() <em>State</em>}' attribute.
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @see #getState()
 	 * @generated
 	 * @ordered
@@ -98,8 +106,9 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 	protected State state = STATE_EDEFAULT;
 
 	/**
-	 * The default value of the '{@link #getStatus() <em>Status</em>}' attribute.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * The default value of the '{@link #getStatus() <em>Status</em>}'
+	 * attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @see #getStatus()
 	 * @generated
 	 * @ordered
@@ -109,6 +118,7 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 	/**
 	 * The cached value of the '{@link #getStatus() <em>Status</em>}' attribute.
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @see #getStatus()
 	 * @generated
 	 * @ordered
@@ -127,6 +137,7 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -138,9 +149,10 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 
 		@Override
 		public void notifyChanged(Notification msg) {
-			if (msg.getFeatureID(getClass()) == ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE)
+			if (msg.getFeatureID(getClass()) == ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE) {
+				setStatus(STATUS_EDEFAULT);
 				setState(State.DYNAMIC);
-
+			}
 			if (msg.getFeatureID(getClass()) == ClassMakerPackage.BUNDLE__STATE
 					&& msg.getNewValue() == State.DYNAMIC
 					&& (getStatus() == null || getStatus().isOK()))
@@ -169,6 +181,7 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 			e.printStackTrace();
 			return;
 		}
+		setStatus(new Status(IStatus.OK, ClassMaker.PLUGIN_ID, "Bundle ready"));		
 		setState(State.GENERATED);
 	}
 
@@ -209,17 +222,20 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public void setName(String newName) {
 		String oldName = name;
 		name = newName;
 		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, ClassMakerPackage.BUNDLE__NAME, oldName, name));
+			eNotify(new ENotificationImpl(this, Notification.SET,
+					ClassMakerPackage.BUNDLE__NAME, oldName, name));
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public EPackage getEPackage() {
@@ -228,6 +244,7 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public NotificationChain basicSetEPackage(EPackage newEPackage,
@@ -235,32 +252,47 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 		EPackage oldEPackage = ePackage;
 		ePackage = newEPackage;
 		if (eNotificationRequired()) {
-			ENotificationImpl notification = new ENotificationImpl(this, Notification.SET, ClassMakerPackage.BUNDLE__EPACKAGE, oldEPackage, newEPackage);
-			if (msgs == null) msgs = notification; else msgs.add(notification);
+			ENotificationImpl notification = new ENotificationImpl(this,
+					Notification.SET, ClassMakerPackage.BUNDLE__EPACKAGE,
+					oldEPackage, newEPackage);
+			if (msgs == null)
+				msgs = notification;
+			else
+				msgs.add(notification);
 		}
 		return msgs;
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public void setEPackage(EPackage newEPackage) {
 		if (newEPackage != ePackage) {
 			NotificationChain msgs = null;
 			if (ePackage != null)
-				msgs = ((InternalEObject)ePackage).eInverseRemove(this, EOPPOSITE_FEATURE_BASE - ClassMakerPackage.BUNDLE__EPACKAGE, null, msgs);
+				msgs = ((InternalEObject) ePackage).eInverseRemove(this,
+						EOPPOSITE_FEATURE_BASE
+								- ClassMakerPackage.BUNDLE__EPACKAGE, null,
+						msgs);
 			if (newEPackage != null)
-				msgs = ((InternalEObject)newEPackage).eInverseAdd(this, EOPPOSITE_FEATURE_BASE - ClassMakerPackage.BUNDLE__EPACKAGE, null, msgs);
+				msgs = ((InternalEObject) newEPackage).eInverseAdd(this,
+						EOPPOSITE_FEATURE_BASE
+								- ClassMakerPackage.BUNDLE__EPACKAGE, null,
+						msgs);
 			msgs = basicSetEPackage(newEPackage, msgs);
-			if (msgs != null) msgs.dispatch();
-		}
-		else if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, ClassMakerPackage.BUNDLE__EPACKAGE, newEPackage, newEPackage));
+			if (msgs != null)
+				msgs.dispatch();
+		} else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET,
+					ClassMakerPackage.BUNDLE__EPACKAGE, newEPackage,
+					newEPackage));
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public EPackage getDynamicEPackage() {
@@ -269,6 +301,7 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public NotificationChain basicSetDynamicEPackage(
@@ -276,32 +309,48 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 		EPackage oldDynamicEPackage = dynamicEPackage;
 		dynamicEPackage = newDynamicEPackage;
 		if (eNotificationRequired()) {
-			ENotificationImpl notification = new ENotificationImpl(this, Notification.SET, ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE, oldDynamicEPackage, newDynamicEPackage);
-			if (msgs == null) msgs = notification; else msgs.add(notification);
+			ENotificationImpl notification = new ENotificationImpl(this,
+					Notification.SET,
+					ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE,
+					oldDynamicEPackage, newDynamicEPackage);
+			if (msgs == null)
+				msgs = notification;
+			else
+				msgs.add(notification);
 		}
 		return msgs;
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public void setDynamicEPackage(EPackage newDynamicEPackage) {
 		if (newDynamicEPackage != dynamicEPackage) {
 			NotificationChain msgs = null;
 			if (dynamicEPackage != null)
-				msgs = ((InternalEObject)dynamicEPackage).eInverseRemove(this, EOPPOSITE_FEATURE_BASE - ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE, null, msgs);
+				msgs = ((InternalEObject) dynamicEPackage).eInverseRemove(this,
+						EOPPOSITE_FEATURE_BASE
+								- ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE,
+						null, msgs);
 			if (newDynamicEPackage != null)
-				msgs = ((InternalEObject)newDynamicEPackage).eInverseAdd(this, EOPPOSITE_FEATURE_BASE - ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE, null, msgs);
+				msgs = ((InternalEObject) newDynamicEPackage).eInverseAdd(this,
+						EOPPOSITE_FEATURE_BASE
+								- ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE,
+						null, msgs);
 			msgs = basicSetDynamicEPackage(newDynamicEPackage, msgs);
-			if (msgs != null) msgs.dispatch();
-		}
-		else if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE, newDynamicEPackage, newDynamicEPackage));
+			if (msgs != null)
+				msgs.dispatch();
+		} else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET,
+					ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE,
+					newDynamicEPackage, newDynamicEPackage));
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public State getState() {
@@ -310,17 +359,20 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public void setState(State newState) {
 		State oldState = state;
 		state = newState == null ? STATE_EDEFAULT : newState;
 		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, ClassMakerPackage.BUNDLE__STATE, oldState, state));
+			eNotify(new ENotificationImpl(this, Notification.SET,
+					ClassMakerPackage.BUNDLE__STATE, oldState, state));
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public IStatus getStatus() {
@@ -329,132 +381,143 @@ public class BundleImpl extends EObjectImpl implements Bundle {
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public void setStatus(IStatus newStatus) {
 		IStatus oldStatus = status;
 		status = newStatus;
 		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, ClassMakerPackage.BUNDLE__STATUS, oldStatus, status));
+			eNotify(new ENotificationImpl(this, Notification.SET,
+					ClassMakerPackage.BUNDLE__STATUS, oldStatus, status));
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public NotificationChain eInverseRemove(InternalEObject otherEnd,
 			int featureID, NotificationChain msgs) {
 		switch (featureID) {
-			case ClassMakerPackage.BUNDLE__EPACKAGE:
-				return basicSetEPackage(null, msgs);
-			case ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE:
-				return basicSetDynamicEPackage(null, msgs);
+		case ClassMakerPackage.BUNDLE__EPACKAGE:
+			return basicSetEPackage(null, msgs);
+		case ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE:
+			return basicSetDynamicEPackage(null, msgs);
 		}
 		return super.eInverseRemove(otherEnd, featureID, msgs);
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public Object eGet(int featureID, boolean resolve, boolean coreType) {
 		switch (featureID) {
-			case ClassMakerPackage.BUNDLE__NAME:
-				return getName();
-			case ClassMakerPackage.BUNDLE__EPACKAGE:
-				return getEPackage();
-			case ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE:
-				return getDynamicEPackage();
-			case ClassMakerPackage.BUNDLE__STATE:
-				return getState();
-			case ClassMakerPackage.BUNDLE__STATUS:
-				return getStatus();
+		case ClassMakerPackage.BUNDLE__NAME:
+			return getName();
+		case ClassMakerPackage.BUNDLE__EPACKAGE:
+			return getEPackage();
+		case ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE:
+			return getDynamicEPackage();
+		case ClassMakerPackage.BUNDLE__STATE:
+			return getState();
+		case ClassMakerPackage.BUNDLE__STATUS:
+			return getStatus();
 		}
 		return super.eGet(featureID, resolve, coreType);
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public void eSet(int featureID, Object newValue) {
 		switch (featureID) {
-			case ClassMakerPackage.BUNDLE__NAME:
-				setName((String)newValue);
-				return;
-			case ClassMakerPackage.BUNDLE__EPACKAGE:
-				setEPackage((EPackage)newValue);
-				return;
-			case ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE:
-				setDynamicEPackage((EPackage)newValue);
-				return;
-			case ClassMakerPackage.BUNDLE__STATE:
-				setState((State)newValue);
-				return;
-			case ClassMakerPackage.BUNDLE__STATUS:
-				setStatus((IStatus)newValue);
-				return;
+		case ClassMakerPackage.BUNDLE__NAME:
+			setName((String) newValue);
+			return;
+		case ClassMakerPackage.BUNDLE__EPACKAGE:
+			setEPackage((EPackage) newValue);
+			return;
+		case ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE:
+			setDynamicEPackage((EPackage) newValue);
+			return;
+		case ClassMakerPackage.BUNDLE__STATE:
+			setState((State) newValue);
+			return;
+		case ClassMakerPackage.BUNDLE__STATUS:
+			setStatus((IStatus) newValue);
+			return;
 		}
 		super.eSet(featureID, newValue);
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public void eUnset(int featureID) {
 		switch (featureID) {
-			case ClassMakerPackage.BUNDLE__NAME:
-				setName(NAME_EDEFAULT);
-				return;
-			case ClassMakerPackage.BUNDLE__EPACKAGE:
-				setEPackage((EPackage)null);
-				return;
-			case ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE:
-				setDynamicEPackage((EPackage)null);
-				return;
-			case ClassMakerPackage.BUNDLE__STATE:
-				setState(STATE_EDEFAULT);
-				return;
-			case ClassMakerPackage.BUNDLE__STATUS:
-				setStatus(STATUS_EDEFAULT);
-				return;
+		case ClassMakerPackage.BUNDLE__NAME:
+			setName(NAME_EDEFAULT);
+			return;
+		case ClassMakerPackage.BUNDLE__EPACKAGE:
+			setEPackage((EPackage) null);
+			return;
+		case ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE:
+			setDynamicEPackage((EPackage) null);
+			return;
+		case ClassMakerPackage.BUNDLE__STATE:
+			setState(STATE_EDEFAULT);
+			return;
+		case ClassMakerPackage.BUNDLE__STATUS:
+			setStatus(STATUS_EDEFAULT);
+			return;
 		}
 		super.eUnset(featureID);
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public boolean eIsSet(int featureID) {
 		switch (featureID) {
-			case ClassMakerPackage.BUNDLE__NAME:
-				return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
-			case ClassMakerPackage.BUNDLE__EPACKAGE:
-				return ePackage != null;
-			case ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE:
-				return dynamicEPackage != null;
-			case ClassMakerPackage.BUNDLE__STATE:
-				return state != STATE_EDEFAULT;
-			case ClassMakerPackage.BUNDLE__STATUS:
-				return STATUS_EDEFAULT == null ? status != null : !STATUS_EDEFAULT.equals(status);
+		case ClassMakerPackage.BUNDLE__NAME:
+			return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT
+					.equals(name);
+		case ClassMakerPackage.BUNDLE__EPACKAGE:
+			return ePackage != null;
+		case ClassMakerPackage.BUNDLE__DYNAMIC_EPACKAGE:
+			return dynamicEPackage != null;
+		case ClassMakerPackage.BUNDLE__STATE:
+			return state != STATE_EDEFAULT;
+		case ClassMakerPackage.BUNDLE__STATUS:
+			return STATUS_EDEFAULT == null ? status != null : !STATUS_EDEFAULT
+					.equals(status);
 		}
 		return super.eIsSet(featureID);
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public String toString() {
-		if (eIsProxy()) return super.toString();
+		if (eIsProxy())
+			return super.toString();
 
 		StringBuffer result = new StringBuffer(super.toString());
 		result.append(" (name: ");
