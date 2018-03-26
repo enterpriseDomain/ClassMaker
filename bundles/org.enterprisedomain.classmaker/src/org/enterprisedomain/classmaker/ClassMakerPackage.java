2c6eecfe6a61f00671ffcc400fe34cb772708d41
Kyrill Zotkin <kyrillzotkin@users.noreply.github.com>
2018-03-26 16:49:54 +0700

    v0.7.920
    Signed-off-by: Kyrill Zotkin <kyrillzotkin@users.noreply.github.com>

--- bundles/org.enterprisedomain.classmaker/src/org/enterprisedomain/classmaker/ClassMakerPackage.java
+++ bundles/org.enterprisedomain.classmaker/src/org/enterprisedomain/classmaker/ClassMakerPackage.java
@@ -50,7 +50,7 @@ public interface ClassMakerPackage extends EPackage {
 	 * 
 	 * @generated
 	 */
-	String eNS_URI = "http://org/enterprisedomain/ClassMaker/0.7.911";
+	String eNS_URI = "http://org/enterprisedomain/ClassMaker/0.7.920";
 
 	/**
 	 * The package namespace name. <!-- begin-user-doc --> <!-- end-user-doc -->
