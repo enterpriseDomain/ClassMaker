782850d29045c771336b8d400d7982157ca59671
Kyrill Zotkin <143617+kyrillzotkin@users.noreply.github.com>
2019-07-22 19:08:14 +0700

    v0.8.0

---
+++ bundles/org.enterprisedomain.workbench.ide/src/org/enterprisedomain/workbench/ide/ModelEditorAssociationOverride.java
@@ -0,0 +1,60 @@
+package org.enterprisedomain.workbench.ide;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.content.IContentType;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.ide.IEditorAssociationOverride;
+
+public class ModelEditorAssociationOverride implements IEditorAssociationOverride {
+
+	@Override
+	public IEditorDescriptor overrideDefaultEditor(IEditorInput editorInput, IContentType contentType,
+			IEditorDescriptor editorDescriptor) {
+		if (contentType.equals(Platform.getContentTypeManager().getContentType("org.eclipse.emf.ecore.xmi"))
+				|| contentType.equals(Platform.getContentTypeManager().getContentType("org.eclipse.emf.ecore")))
+			return PlatformUI.getWorkbench().getEditorRegistry().findEditor(ModelEditor.EDITOR_ID);
+		return null;
+	}
+
+	@Override
+	public IEditorDescriptor overrideDefaultEditor(String fileName, IContentType contentType,
+			IEditorDescriptor editorDescriptor) {
+		if (fileName.endsWith("xmi") || fileName.endsWith("ecore"))
+			return PlatformUI.getWorkbench().getEditorRegistry().findEditor(ModelEditor.EDITOR_ID);
+		return null;
+	}
+
+	@Override
+	public IEditorDescriptor[] overrideEditors(IEditorInput editorInput, IContentType contentType,
+			IEditorDescriptor[] editorDescriptors) {
+		List<IEditorDescriptor> results = new ArrayList<IEditorDescriptor>();
+		for (IEditorDescriptor e : editorDescriptors)
+			results.add(e);
+		if (contentType.equals(Platform.getContentTypeManager().getContentType("org.eclipse.emf.ecore.xmi"))
+				|| contentType.equals(Platform.getContentTypeManager().getContentType("org.eclipse.emf.ecore"))) {
+			results.add(PlatformUI.getWorkbench().getEditorRegistry().findEditor(ModelEditor.EDITOR_ID));
+		}
+		return results.toArray(new IEditorDescriptor[results.size()]);
+	}
+
+	@Override
+	public IEditorDescriptor[] overrideEditors(String fileName, IContentType contentType,
+			IEditorDescriptor[] editorDescriptors) {
+		List<IEditorDescriptor> results = new ArrayList<IEditorDescriptor>();
+		for (IEditorDescriptor e : editorDescriptors)
+			results.add(e);
+		if (fileName.endsWith("xmi") || fileName.endsWith("ecore")) {
+			results.add(PlatformUI.getWorkbench().getEditorRegistry().findEditor(ModelEditor.EDITOR_ID));
+		}
+		return results.toArray(new IEditorDescriptor[results.size()]);
+	}
+
+}
